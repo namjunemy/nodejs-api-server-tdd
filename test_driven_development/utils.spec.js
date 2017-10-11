a9b218efe4a161983e7b4292f07af53501076e6c
Namjun Kim <namjunemy@gmail.com>
2017-10-12 02:13:40 +0900

    should 라이브러리 사용 테스트 코드 변경

--- test_driven_development/utils.spec.js
+++ test_driven_development/utils.spec.js
@@ -1,9 +1,9 @@
 const utils = require('./utils');
-const assert = require('assert');
+const should = require('should');
 
 describe('utils.js모듈의 capitalize() 함수는', () => {
   it('문자열의 첫번쨰 문자를 대문자로 변환한다', () => {
     const result = utils.capitalize('hello');
-    assert.equal(result,'Hello');
+    result.should.be.equal('Hello');
   });
 });
