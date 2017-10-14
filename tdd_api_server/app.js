caea29f4d0c8ba33df0212deb481b1c3e0592522
Namjun Kim <namjunemy@gmail.com>
2017-10-14 15:24:48 +0900

    사용자 목록 조회 API 테스트 코드 생성

---
+++ tdd_api_server/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+let users = [
+  {id: 1, name: 'alice'},
+  {id: 2, name: 'bek'},
+  {id: 3, name: 'chris'}
+];
+
+app.use(morgan('dev'));
+
+app.get('/users', function (req, res) {
+  req.query.limit = req.query.limit || 10;
+  const limit = parseInt(req.query.limit, 10);
+  if (Number.isNaN(limit)) {
+    return res.status(400).end();
+  }
+  res.json(users.slice(0, limit));
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+module.exports = app;
