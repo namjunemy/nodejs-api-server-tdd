46a1edd4d143005075cfbf69e1d526520918b164
Namjun Kim <namjunemy@gmail.com>
2017-10-16 00:49:54 +0900

    코드 리팩토링 - 라우터 클래스 분리

--- tdd_api_server/app.js
+++ tdd_api_server/app.js
@@ -2,82 +2,13 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-
-let users = [
-  {id: 1, name: 'alice'},
-  {id: 2, name: 'bek'},
-  {id: 3, name: 'chris'}
-];
+const user = require('./api/user/index');
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/users', (req, res) => {
-  req.query.limit = req.query.limit || 10;
-  const limit = parseInt(req.query.limit, 10);
-  if (Number.isNaN(limit)) {
-    return res.status(400).end();
-  }
-  res.json(users.slice(0, limit));
-});
-
-app.get('/users/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  if (Number.isNaN(id))
-    return res.status(400).end();
-
-  const user = users.filter((user) => user.id === id)[0];
-  if (!user)
-    return res.status(404).end();
-
-  res.json(user);
-});
-
-app.delete('/users/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  if (Number.isNaN(id))
-    return res.status(400).end();
-  users = users.filter(user => user.id !== id);
-  res.status(204).end();
-});
-
-app.post('/users', (req, res) => {
-  const name = req.body.name;
-  if (!name)
-    return res.status(400).end();
-
-  const isConflict = users.filter(user => user.name === name).length;
-  if (isConflict)
-    return res.status(409).end();
-
-  const id = Date.now() + 1;
-  const user = {id, name};
-  users.push(user);
-  res.status(201).json(user);
-});
-
-app.put('/users/:id', (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  if (Number.isNaN(id))
-    return res.status(400).end();
-
-  const name = req.body.name;
-  if (!name)
-    return res.status(400).end();
-
-  const isConfilct = users.filter(user => user.name === name).length;
-  if (isConfilct)
-    return res.status(409).end();
-
-  const user = users.filter((user) => user.id === id)[0];
-  if (!user)
-    return res.status(404).end();
-
-  user.name = name;
-
-  res.json(user);
-});
+app.use('/users', user);
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
